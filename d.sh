d3deb3fcd49f57778b85c6c0af8e5395c5b1460f
dgdsingen <dgdsingen@gmail.com>
2024-07-30 10:54:06 +0900

    d.sh

--- d.sh
+++ d.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# git config --global --add safe.directory /tmp/playbook
-# git config core.pager cat
+export
 
-# git add .;
 PRE_TAG=$(git tag -l | tail -1)
 git diff "$PRE_TAG" --color=always
-
-git restore --staged .
