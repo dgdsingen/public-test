e7ff4a2ec3edd197f4ddf37d55fe3a54b19c9ecf
dgdsingen <dgdsingen@gmail.com>
2024-08-01 19:08:46 +0900

    update

--- prompt.sh
+++ prompt.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo -n "### param ###"
+echo $@
+echo -n "### param ###"
+
+echo -n "### env ###"
+export
+echo -n "### env ###"
+
 echo -n "# proceed? [y/n] "
 read proceed
 if [[ "$proceed" == "y" ]]; then
